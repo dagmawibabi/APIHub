9010572af0a33143ca5829a23003bffd8c54ec9b
Henok Ayenew <henacodes@gmail.com>
2024-03-03 13:29:34 +0300

    added documentation project

---
+++ documentation/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>API-HUB</span>,
+  project: {
+    link: 'https://github.com/dagmawibabi/apihub',
+  },
+  docsRepositoryBase: 'https://github.com/henacodes/apihub-docs',
+  footer: {
+    text: 'API Hub Documentation',
+  },
+}
+
+export default config
